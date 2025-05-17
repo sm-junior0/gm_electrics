82e04f32470882c8c52590818e22c10846c8c862
Manene <juniormanene@gmail.com>
2025-05-17 20:58:23 +0200

    footer links

--- src/components/TeamSection.tsx
+++ src/components/TeamSection.tsx
@@ -27,7 +27,7 @@ const teamMembers = [
 
 const TeamSection = () => {
   return (
-    <section className="py-16 sm:py-24 bg-gray-50">
+    <section id="team" className="py-16 sm:py-24 bg-gray-50">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         {/* Section Header */}
         <div className="text-center mb-16">
